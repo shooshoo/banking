8ebd497978efb2b58bcbf2d5a586948b3799b770
Shili Manchester <shilan_hk@yahoo.com>
2011-10-06 15:18:34 +0100

    Fails with withdrawal

--- specs/account_spec.rb
+++ specs/account_spec.rb
@@ -14,10 +14,24 @@ describe Account do
     subject.balance.should==starting_balance
   end
 
-  it "it should add the deposit value to the balance" do
+  it "should add the deposit value to the balance" do
     subject=Account.new
     subject.balance.should==0.0
     subject.deposit(100.0)
     subject.balance.should==100.0
   end
+   it "should decrease the balance by the amount of withdrawal"  do
+   subject=Account.new(100.0)
+    subject.balance.should==100.0
+    subject.withdrawal(100.0)
+    subject.balance.should==0.0
+   end
+
+  #it "should check that the balance is not less than zero" do
+  #  subject=Account.new
+  #  subject.balance.should==0.0
+  #  subject.deposit(-10.0).should raise_error
+  #
+  #
+  #end
 end
