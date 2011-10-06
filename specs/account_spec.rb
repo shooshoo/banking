8e0385a8dcc6f0509556290c50936f476dc2a5f0
Shili Manchester <shilan_hk@yahoo.com>
2011-10-06 14:51:10 +0100

    balance passes it's first test after instantiating an Account which contains 0.0

--- specs/account_spec.rb
+++ specs/account_spec.rb
@@ -1,10 +1,10 @@
-require "rspec"
+require 'rspec'
+require '../lib/account.rb'
 
-describe "My behaviour" do
+describe Account do
 
-  it "should do something" do
-
-    #To change this template use File | Settings | File Templates.
-    true.should == false
+  it "should initially have a balance of zero" do
+      account=Account.new
+      account.balance.should==0.0
   end
 end
